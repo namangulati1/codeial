bc65072aaa1d0ea55e6b20af551c0c335b56c70d
Naman <gulati.naman98@gmail.com>
2023-03-08 20:48:08 +0530

    Add new project to my repo

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,64 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+
+// authentication using passport
+passport.use(new LocalStrategy({
+        usernameField: 'email'
+    },
+    function(email, password, done){
+        // find a user and establish the identity
+        User.findOne({email: email}, function(err, user)  {
+            if (err){
+                console.log('Error in finding user --> Passport');
+                return done(err);
+            }
+
+            if (!user || user.password != password){
+                console.log('Invalid Username/Password');
+                return done(null, false);
+            }
+
+            return done(null, user);
+        });
+    }
+
+
+));
+
+
+// serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        if(err){
+            console.log('Error in finding user --> Passport');
+            return done(err);
+        }
+
+        return done(null, user);
+    });
+});
+
+//check if the user is authenticated
+passport.checkAuthentication = function(req,res,next) {
+    //if the user is signed in, then pass on the req to the next function (controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+    //if the user is not signned in 
+    return res.redirect('/users/sign-in');
+}
+
+
+
+module.exports = passport;
