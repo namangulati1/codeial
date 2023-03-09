cbdfb2a3a4709c62ce1fe06a4d3f354327fae4d7
Naman <gulati.naman98@gmail.com>
2023-03-09 18:09:28 +0530

    Setting up connect mongo

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -64,6 +64,8 @@ passport.setAuthenticatedUser = function(req,res,next){
         //req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
         res.locals.user = req.user;
     }
+
+    next();
 }
 
 
